8b3b1c7e2b6938fd90d2566710180ca046427707
Sukhar1922 <s.a.v.04@ya.ru>
2026-03-26 20:28:44 +0300

    django secret key in env

--- server/Messenger/Messenger/settings.py
+++ server/Messenger/Messenger/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2j)&&(7vu#wgp-2j0m%uik@^v$f98v*l%d1nyz^te!kb)8^o^#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
